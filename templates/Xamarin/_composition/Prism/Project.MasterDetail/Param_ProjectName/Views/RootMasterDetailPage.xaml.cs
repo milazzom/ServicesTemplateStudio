12dee1e7a322d3d7a50ba6b878d73a133f96df3c
Lee Parrish <lparrish@microsoft.com>
2018-04-07 11:40:52 -0500

    Base Xamarin Template with Shared Project

---
+++ templates/Xamarin/_composition/Prism/Project.MasterDetail/Param_ProjectName/Views/RootMasterDetailPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Param_RootNamespace.Models;
+
+namespace Param_RootNamespace.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RootMasterDetailPage : Xamarin.Forms.MasterDetailPage
+    {
+        public RootMasterDetailPage()
+        {
+            InitializeComponent();
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                MasterBehavior = MasterBehavior.Popover;
+            }
+        }
+    }
+}
