fc9cd02e7640379b8bab8cd7500f7d1f9d761034
Lee Parrish <lparrish@microsoft.com>
2018-04-07 11:40:52 -0500

    Base Xamarin Template with Shared Project

---
+++ templates/Xamarin/_composition/Prism/Project.MasterDetail/Param_ProjectName/ViewModels/RootMasterDetailPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using Param_RootNamespace.Models;
+using Param_RootNamespace.Views;
+using Prism.Navigation;
+using Prism.Commands;
+
+namespace Param_RootNamespace.ViewModels
+{
+    public class RootMasterDetailPageViewModel : ViewModelBase
+    {
+        public RootMasterDetailPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            NavigateCommand = new DelegateCommand(Navigate);
+        }
+
+        private MasterDetailPageMenuItem selectedMenuItem;
+        public MasterDetailPageMenuItem SelectedMenuItem
+        {
+            get => selectedMenuItem;
+            set => SetProperty(ref selectedMenuItem, value);
+        }
+
+        private async void Navigate()
+        {
+            await NavigationService.NavigateAsync($"NavigationPage/{SelectedMenuItem.TargetPage}");
+        }
+
+        public DelegateCommand NavigateCommand { get; private set; }
+
+        public ObservableCollection<MasterDetailPageMenuItem> PrimaryMenuItems { get; private set; } = new ObservableCollection<MasterDetailPageMenuItem>
+        {
+        };
+    }
+}
