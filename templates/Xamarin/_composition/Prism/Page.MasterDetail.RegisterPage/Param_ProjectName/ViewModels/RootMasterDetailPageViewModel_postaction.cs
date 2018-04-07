c14b4d6e013b06d6150a023347e483ce2ce47402
Lee Parrish <lparrish@microsoft.com>
2018-04-07 11:40:52 -0500

    Base Xamarin Template with Shared Project

---
+++ templates/Xamarin/_composition/Prism/Page.MasterDetail.RegisterPage/Param_ProjectName/ViewModels/RootMasterDetailPageViewModel_postaction.cs
@@ -0,0 +1,13 @@
+﻿namespace Param_RootNamespace.ViewModels
+{
+    public class RootMasterDetailPageViewModel : ViewModelBase
+    {
+        public ObservableCollection<MasterDetailPageMenuItem> PrimaryMenuItems { get; private set; } = new ObservableCollection<MasterDetailPageMenuItem>
+        {
+            //^^
+            //{[{
+            new MasterDetailPageMenuItem { Title = "wts.ItemName", TargetPage = "wts.ItemNamePage", IconSource = "blank.png"},
+            //}]}
+        };
+    }
+}
---
+++ templates/Xamarin/_composition/Prism/Page.Navigation.RegisterPage/Param_ProjectName/App_postaction.xaml.cs
@@ -0,0 +1,18 @@
+﻿//{[{
+using Param_RootNamespace.Views;
+using Param_RootNamespace.ViewModels;
+//}]}
+namespace Param_RootNamespace
+{
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            //^^
+            //{[{
+            containerRegistry.RegisterForNavigation<wts.ItemNamePage>();
+            //}]}
+        }
+    }
+}
