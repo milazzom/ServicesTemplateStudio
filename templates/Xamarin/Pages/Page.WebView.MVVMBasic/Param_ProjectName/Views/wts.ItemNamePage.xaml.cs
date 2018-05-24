ec19dea3e4e95736f9f62cb1d25f9d1c346396bd
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2018-05-23 21:07:54 -0700

    First attempt at building the WebView template for Xamarin.Forms.

---
+++ templates/Xamarin/Pages/Page.WebView.MVVMBasic/Param_ProjectName/Views/wts.ItemNamePage.xaml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Param_ProjectName.Services;
+
+using Param_ProjectName.ViewModels;
+
+namespace Param_ProjectName.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class wts.ItemNamePage : ContentPage
+	{
+        private wts.ItemNameViewModel ViewModel;
+		public wts.ItemNamePage ()
+		{
+			InitializeComponent ();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(On<Xamarin.Forms.PlatformConfiguration.iOS>(), true);
+            ViewModel = new wts.ItemNamePageViewModel(_webView);
+            ViewModel.Url = "https://developer.microsoft.com";
+            ViewModel.GoCommand.Execute(null);
+            BindingContext = ViewModel;
+            BackButton.SetBinding(Button.CommandProperty, nameof(ViewModel.BackCommand));
+            ForwardButton.SetBinding(Button.CommandProperty, nameof(ViewModel.ForwardCommand));
+            UrlEntry.SetBinding(Entry.TextProperty, nameof(ViewModel.Url));
+            GoButton.SetBinding(Button.CommandProperty, nameof(ViewModel.GoCommand));
+            GoButton.Text = DependencyService.Get<ILocalizer>().GetStringForKey("wts.ItemNameGoButton");
+            BackButton.Text = DependencyService.Get<ILocalizer>().GetStringForKey("wts.ItemNameBackButton");
+            ForwardButton.Text = DependencyService.Get<ILocalizer>().GetStringForKey("wts.ItemNameForwardButton");
+		}
+
+
+	}
+}
