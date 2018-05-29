c3224cd9247b0b6e672daf11d7b53a9396cba9e3
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2018-05-28 21:25:42 -0700

    Adds POC app named App19 to allow for distributed troubleshooting for UWP localization.

---
+++ poc/StsXamarin/Localization/App19/App19/App19/Services/ILocalizer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App19.Services
+{
+    public interface ILocalizer
+    {
+        string GetStringForKey(string key);
+
+    }
+}
