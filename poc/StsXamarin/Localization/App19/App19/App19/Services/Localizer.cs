085e8f7fb21ab7941ddc2c4f2b423614f78e1c15
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2018-05-28 21:25:42 -0700

    Adds POC app named App19 to allow for distributed troubleshooting for UWP localization.

---
+++ poc/StsXamarin/Localization/App19/App19/App19/Services/Localizer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(App19.Services.Localizer))]
+namespace App19.Services
+{
+    public class Localizer : ILocalizer
+    {
+        private const string resourceName = "App19.Strings.AppResources";
+        static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(() => new ResourceManager(resourceName, IntrospectionExtensions.GetTypeInfo(typeof(Localizer)).Assembly));
+
+
+        public string GetStringForKey(string key)
+        {
+            string text = string.Empty;
+            var currentCulture = DependencyService.Get<ILocalizationInfo>().GetCurrentCultureInfo();
+            if(currentCulture == null)
+            {
+                currentCulture = CultureInfo.CurrentUICulture;
+            }
+            text = ResMgr.Value.GetString(key, currentCulture);
+            return text;
+        }
+    }
+}
---
+++ poc/StsXamarin/Localization/App19/App19/App19/Views/WebViewPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using App19.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App19.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class WebViewPage : ContentPage
+	{
+
+        public WebViewPage ()
+		{
+			InitializeComponent ();
+            GoButton.Text = DependencyService.Get<ILocalizer>().GetStringForKey("WebViewGoButton");
+            ForwardButton.Text = DependencyService.Get<ILocalizer>().GetStringForKey("WebViewForwardButton");
+            BackButton.Text = DependencyService.Get<ILocalizer>().GetStringForKey("WebViewBackButton");
+            UrlEntry.Text = "https://developer.microsoft.com";
+            _webView.Source = new UrlWebViewSource()
+            {
+                Url = UrlEntry.Text
+            };
+            UpdateButtonStates();
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GoButton.Clicked += GoButton_Clicked;
+            BackButton.Clicked += BackButton_Clicked;
+            ForwardButton.Clicked += ForwardButton_Clicked;
+            _webView.Navigated += OnNavigated;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            GoButton.Clicked -= GoButton_Clicked;
+            BackButton.Clicked -= BackButton_Clicked;
+            ForwardButton.Clicked -= ForwardButton_Clicked;
+            _webView.Navigated -= OnNavigated;
+        }
+
+        private void OnNavigated(object sender, WebNavigatedEventArgs e)
+        {
+            UrlEntry.Text = e.Url;
+        }
+
+        private void ForwardButton_Clicked(object sender, EventArgs e)
+        {
+            if(_webView.CanGoForward)
+            {
+                _webView.GoForward();
+            }
+            UpdateButtonStates();
+        }
+
+        private void UpdateButtonStates()
+        {
+            ForwardButton.IsEnabled = _webView.CanGoForward;
+            BackButton.IsEnabled = _webView.CanGoBack;
+        }
+
+        private void BackButton_Clicked(object sender, EventArgs e)
+        {
+            if(_webView.CanGoBack)
+            {
+                _webView.GoBack();
+            }
+            UpdateButtonStates();
+        }
+
+        private void GoButton_Clicked(object sender, EventArgs e)
+        {
+            var url = UrlEntry.Text;
+            if(!string.IsNullOrEmpty(url))
+            {
+                _webView.Source = new UrlWebViewSource()
+                {
+                    Url = url
+                };
+                UpdateButtonStates();
+            }
+        }
+    }
+}
