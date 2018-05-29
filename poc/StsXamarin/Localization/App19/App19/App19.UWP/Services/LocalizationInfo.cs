fc5ff3fbe5013b4f7a7002ac273009fb52cc1774
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2018-05-28 21:25:42 -0700

    Adds POC app named App19 to allow for distributed troubleshooting for UWP localization.

---
+++ poc/StsXamarin/Localization/App19/App19/App19.UWP/Services/LocalizationInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using Xamarin.Forms;
+using App19.Services;
+
+[assembly: Dependency(typeof(App19.UWP.Services.LocalizationInfo))]
+namespace App19.UWP.Services
+{
+    class LocalizationInfo : ILocalizationInfo
+    {
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            return CultureInfo.CurrentUICulture;
+        }
+
+        public void SetLocale(CultureInfo ci)
+        {
+            CultureInfo.CurrentCulture = ci;
+        }
+    }
+}
