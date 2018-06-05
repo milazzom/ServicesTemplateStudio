12c9ebfd2fef8e5368b78913fe4d818ff3cf7337
Lee Parrish <lparrish@microsoft.com>
2018-06-04 21:05:32 -0500

    Fix odd spacing on Blank page code behind.

--- templates/Xamarin/Pages/Blank.Prism/Param_ProjectName/Views/BlankViewPage.xaml.cs
+++ templates/Xamarin/Pages/Blank.Prism/Param_ProjectName/Views/BlankViewPage.xaml.cs
@@ -6,9 +6,9 @@ namespace Param_ItemNamespace.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BlankViewPage : ContentPage
     {
-        public BlankViewPage ()
+        public BlankViewPage()
         {
-            InitializeComponent ();
+            InitializeComponent();
         }
     }
 }
