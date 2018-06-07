d0ab1d77aa15ab5ba0555c5bcae3ceb10c78ae29
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2018-06-07 13:20:34 -0700

    Resolves issues with the web view not compiling due to a missing variable name.

---
+++ templates/Xamarin/Features/Localization/Param_ProjectName/Views/LocalizationExamplePage.xaml.cs
@@ -0,0 +1,18 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Param_ItemNamespace.ViewModels;
+
+namespace Param_ItemNamespace.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LocalizationExamplePage : ContentPage
+    {
+        public LocalizationExamplePage ()
+        {
+            InitializeComponent ();
+            BindingContext = new LocalizationExampleViewModel();
+            GreetingLabel.SetBinding(Label.TextProperty, )
+        }
+    }
+}
