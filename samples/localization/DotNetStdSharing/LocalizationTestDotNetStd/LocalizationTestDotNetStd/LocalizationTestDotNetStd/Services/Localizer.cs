4888b6c91540dc89443dae0a4d6d32bdda2f3dc2
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2018-06-11 18:19:57 -0700

    Adds sample projects for localization.  SharedProject solution for localization does not work properly, but is being added to allow for remote review.

---
+++ samples/localization/DotNetStdSharing/LocalizationTestDotNetStd/LocalizationTestDotNetStd/LocalizationTestDotNetStd/Services/Localizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(LocalizationTestDotNetStd.Services.Localizer))]
+namespace LocalizationTestDotNetStd.Services
+{
+    public class Localizer : ILocalizer
+    {
+        private const string resourceName = "LocalizationTestDotNetStd.Strings.AppResources";
+        static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(() => new ResourceManager(resourceName, IntrospectionExtensions.GetTypeInfo(typeof(Localizer)).Assembly));
+
+
+        public string GetStringForKey(string key)
+        {
+            string text = string.Empty;
+            var currentCulture = DependencyService.Get<ILocalizationInfo>().GetCurrentCultureInfo();
+            if (currentCulture == null)
+            {
+                currentCulture = CultureInfo.CurrentUICulture;
+            }
+            text = ResMgr.Value.GetString(key, currentCulture);
+            return text;
+        }
+    }
+}
---
+++ samples/localization/SharedProject/LocalizationTest/LocalizationTest/LocalizationTest/Services/Localizer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(LocalizationTest.Services.Localizer))]
+namespace LocalizationTest.Services
+{
+    public class Localizer : ILocalizer
+    {
+        
+        private static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(() => new ResourceManager($"{Assembly.GetExecutingAssembly().GetName().Name}.Strings.AppResources", IntrospectionExtensions.GetTypeInfo(typeof(Localizer)).Assembly));
+
+        public string GetStringForKey(string key)
+        {
+            var resourceName = $"{Assembly.GetExecutingAssembly().GetName().Name}";
+            string text = string.Empty;
+            var currentCulture = DependencyService.Get<ILocalizationInfo>().GetCurrentCultureInfo();
+            if (currentCulture == null)
+            {
+                currentCulture = CultureInfo.CurrentUICulture;
+            }
+            try
+            {
+                text = ResMgr.Value.GetString(key, currentCulture);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                text = key;
+            }
+            return text;
+        }
+    }
+}
