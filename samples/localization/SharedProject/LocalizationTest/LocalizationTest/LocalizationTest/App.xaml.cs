9fa61b27387831fb748ac4f4e6b293d2c9a5e9f9
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2018-06-11 18:19:57 -0700

    Adds sample projects for localization.  SharedProject solution for localization does not work properly, but is being added to allow for remote review.

---
+++ samples/localization/SharedProject/LocalizationTest/LocalizationTest/LocalizationTest/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using LocalizationTest.Views;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace LocalizationTest
+{
+	public partial class App : Application
+	{
+		
+		public App ()
+		{
+			InitializeComponent();
+
+
+			MainPage = new LocalizationTestPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
