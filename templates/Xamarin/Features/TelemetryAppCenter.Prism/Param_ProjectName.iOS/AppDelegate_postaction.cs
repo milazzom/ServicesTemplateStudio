8170924729e18d6e9ae285d7766ca269e3d095b5
Jeremy Woo-Sam <jeremyws@microsoft.com>
2018-06-13 12:26:16 -0700

    AppCenter Xamarin Implementation. Updated to NETStd project to add a tag for references

---
+++ templates/Xamarin/Features/TelemetryAppCenter.Prism/Param_ProjectName.iOS/AppDelegate_postaction.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Ioc;
+//^^
+//{[{
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Param_RootNamespace.Services;
+using Param_RootNamespace.iOS.Services;
+//}]}
+
+namespace Param_RootNamespace.iOS
+{
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+//{[{   
+        private readonly string AppCenterId = string.Empty; // TODO - add Hockey App ID
+//}]}
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+//{[{           
+            InitializeTelemetry();
+//}]}
+        }
+//{[{
+    
+        private void InitializeTelemetry()
+        {
+            if (!string.IsNullOrEmpty(AppCenterId))
+            {
+                Microsoft.AppCenter.AppCenter.Start(AppCenterId, typeof(Analytics), typeof(Crashes));
+            }
+        }
+//}]}
+    }
+
+    public class iOSInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry container)
+        {
+//{[{
+            container.Register(typeof(ITrackingAPI), typeof(TrackingAPI));
+//}]}
+        }
+    }
+}
