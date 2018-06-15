aa9735950571d05eae041a36c310f0864ba4f72f
Jeremy Woo-Sam <jeremyws@microsoft.com>
2018-06-15 13:44:38 -0700

    Task 356 Add a Xamarin Telemetry Page template to demonstrate Telemetry Service Feature

--- templates/Xamarin/Features/TelemetryAppCenter.Prism/Param_ProjectName/App_postaction.xaml.cs
+++ templates/Xamarin/Features/TelemetryAppCenter.Prism/Param_ProjectName/App_postaction.xaml.cs
@@ -4,7 +4,6 @@
 using Param_RootNamespace.Services;
 //}]}
 
-
 namespace Param_RootNamespace
 {
     public partial class App : PrismApplication
---
+++ templates/Xamarin/Pages/TelemetryExample.Prism/Param_ProjectName/ViewModels/TelemetryViewPageViewModel.cs
@@ -0,0 +1,183 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Param_ItemNamespace.Services;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+namespace Param_ItemNamespace.ViewModels
+{
+	public class TelemetryViewPageViewModel : ViewModelBase
+    {
+        public Boolean TrackEventToggle { get; set; }
+        public Boolean TrackDependencyToggle { get; set; }
+        public Boolean TrackExceptionToggle { get; set; }
+        public Boolean TrackMetricToggle { get; set; }
+        public Boolean TrackPageToggle { get; set; }
+        public Boolean TrackedTimeEventToggle { get; set; }
+        public Boolean TrackAggregatedMetricToggle { get; set; }
+        public int MetricLoggingDelay { get; set; }
+        public  string LoggingLevelItem { get; set; }
+
+        public DelegateCommand TelemetryCommand { get; private set; }
+        public TelemetryViewPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            TelemetryCommand = new DelegateCommand(OnTelemetryCommand);
+            TrackEventToggle = true;
+            TrackDependencyToggle = true;
+            TrackExceptionToggle = true;
+            TrackMetricToggle = true;
+            TrackPageToggle = true;
+            TrackedTimeEventToggle = true;
+            TrackAggregatedMetricToggle = false;
+            MetricLoggingDelay = TrackingService.AggregateMetricLoggingDelay;
+            LoggingLevelItem = Enum.GetName(typeof(TrackingLoggingLevel), TrackingService.AppLoggingLevel);
+        }
+
+        private void OnTelemetryCommand()
+        {
+            TrackingService.AggregateMetricLoggingDelay = MetricLoggingDelay;
+            TrackingService.AppLoggingLevel = (TrackingLoggingLevel)Enum.Parse(typeof(TrackingLoggingLevel), LoggingLevelItem);
+ 
+            //TrackEvent
+            if (TrackEventToggle)
+            {
+                DoTrackEvent(TrackingService);
+            }
+
+            //TrackDependency
+            if (TrackDependencyToggle)
+            {
+                DoTrackDependency(TrackingService);
+            }
+
+            //TrackException
+            if (TrackExceptionToggle)
+            {
+                DoTrackException(TrackingService);
+            }
+
+            //TrackMetric
+            if (TrackMetricToggle)
+            {
+                DoTrackMetric(TrackingService);
+            }
+            //TrackPage
+            if (TrackPageToggle)
+            {
+                DoTrackPage(TrackingService);
+            }
+
+            //Using 
+            if (TrackedTimeEventToggle)
+            {
+                System.Diagnostics.Debug.WriteLine("Track Timed Event");
+                using (new TrackedTimedEvent("DoStuff", TrackingService))
+                {
+                    var rand = new Random();
+                    int delay = rand.Next(100, 500);
+                    Task.Delay(delay).Wait();
+                    /* Do stuff */
+                }
+            }
+
+            //TrackAggregateMetric
+            if (TrackAggregatedMetricToggle)
+            {
+                DoTrackAggregateMetricAsync(TrackingService);
+            }
+        }
+
+        private void DoTrackPage(ITrackingService trackingService)
+        {
+            System.Diagnostics.Debug.WriteLine("Track Page");
+            trackingService.TrackPageView("DoTrackPageView method");
+        }
+
+        private async void DoTrackAggregateMetricAsync(ITrackingService trackingService)
+        {
+            System.Diagnostics.Debug.WriteLine("Track Aggregate Metric - Started");
+
+            var rand = new Random();
+            int i = 0;
+            for (i = 0; i < 1000; i++)
+            {
+                int delay = rand.Next(100, 500);
+                double randomVal = rand.NextDouble();
+
+                await Task.Delay(delay);
+
+                trackingService.TrackAggregateMetric("My Fast Metric", delay, null, TimeSpan.FromMilliseconds(delay));
+
+                var parameters = new Dictionary<string, string>
+                {
+                    {"delay", delay.ToString() },
+                    {"double", randomVal.ToString() }
+                };
+
+                trackingService.TrackAggregateMetric("My Fast Double Metric", randomVal, parameters, TimeSpan.FromMilliseconds(delay), TrackingLoggingLevel.Information);
+            }
+            System.Diagnostics.Debug.WriteLine("Track Aggregate Metric - Completed");
+        }
+
+        private void DoTrackMetric(ITrackingService trackingService)
+        {
+            var rand = new Random();
+            System.Diagnostics.Debug.WriteLine("Track Metric");
+            trackingService.TrackMetric("DoTrackMetric", rand.NextDouble(), null);
+
+            var parameters = new Dictionary<string, string>
+            {
+                {"param1", "value1" },
+                {"param2", "value2" }
+            };
+            trackingService.TrackMetric("DoTrackMetric method with parameters - AppLoggingLevel is verbose", rand.NextDouble(), parameters);
+        }
+
+        private void DoTrackException(ITrackingService trackingService)
+        {
+            System.Diagnostics.Debug.WriteLine("Track Exception");
+            try
+            {
+                throw new Exception("Test Exception");
+            }
+            catch (Exception e) // handled exception
+            {
+                System.Diagnostics.Debug.WriteLine($"DoTrackException {e.Message}");
+                trackingService.TrackException(e, null);
+            }
+
+            try
+            {
+                throw new Exception("Test Exception with Parameters");
+            }
+            catch (Exception e) // handled exception
+            {
+                System.Diagnostics.Debug.WriteLine($"DoTrackException {e.Message}");
+                var parameters = new Dictionary<string, string>()
+                {
+                    {"Param1", "Value1" },
+                    {"Param2", "Value2" }
+                };
+                trackingService.TrackException(e, parameters);
+            }
+        }
+
+        private void DoTrackDependency(ITrackingService trackingService)
+        {
+            System.Diagnostics.Debug.WriteLine("Track Dependency");
+            var rand = new Random();
+            int delay = rand.Next(100, 1000);
+            trackingService.TrackDependency("DoTrackDependency method", "DoTrackDependency", DateTime.Now, TimeSpan.FromMilliseconds(delay), true);
+        }
+
+        private void DoTrackEvent(ITrackingService trackingService)
+        {
+            System.Diagnostics.Debug.WriteLine("Track Event");
+            trackingService.TrackEvent("DoTrackEvent method");
+        }
+    }
+}
