b088d43952afca89c54a762ffe8a56fd53c22d27
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2018-06-16 12:23:42 -0700

    Adds support for localization and web view for the PrismSharedProject framework.

--- templates/Xamarin/Features/Localization.Prism/Param_ProjectName/Services/Localizer.cs
+++ templates/Xamarin/Features/Localization.Prism/Param_ProjectName/Services/Localizer.cs
@@ -7,10 +7,13 @@ using System.Text;
 
 namespace Param_ProjectName.Services
 {
+    /// <summary>
+    /// Class that resolves references to RESX files for use in application localization.
+    /// </summary>
     public class Localizer : ILocalizer
     {
-        private const string resourceName = "Param_ProjectName.Strings.AppResources";
-        static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(() => new ResourceManager(resourceName, IntrospectionExtensions.GetTypeInfo(typeof(Localizer)).Assembly));
+        private static string resourceName = GetResourceId();
+        private static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(() => new ResourceManager(resourceName, IntrospectionExtensions.GetTypeInfo(typeof(Localizer)).Assembly));
         private ILocalizationInfo localizationInfo;
 
         public Localizer(ILocalizationInfo localizationInfo)
@@ -18,6 +21,20 @@ namespace Param_ProjectName.Services
             this.localizationInfo = localizationInfo;
         }
 
+        /// <summary>
+        /// Method to obtain the correct platform-specific resource ID for the AppResources file.
+        /// </summary>
+        /// <returns>Resource ID for AppResources.resx for the currently running platform.</returns>
+        private static string GetResourceId()
+        {
+            return resourceName;
+        }
+
+        /// <summary>
+        /// Method to obtain a string from the AppResources RESX file associated with the current culture.
+        /// </summary>
+        /// <param name="key">Name of the key</param>
+        /// <returns>String value corresponding to the specified key</returns>s
         public string GetStringForKey(string key)
         {
             string text = string.Empty;
