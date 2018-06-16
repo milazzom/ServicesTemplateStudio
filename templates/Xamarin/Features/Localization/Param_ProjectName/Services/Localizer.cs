c5d6e7cdb55873ac10ce79a6c2ca15db646e205d
Michael Milazzo <1437369+milazzom@users.noreply.github.com>
2018-06-15 23:26:54 -0700

    Updates the original localization template to try to work with shared projects.

--- templates/Xamarin/Features/Localization/Param_ProjectName/Services/Localizer.cs
+++ templates/Xamarin/Features/Localization/Param_ProjectName/Services/Localizer.cs
@@ -11,10 +11,27 @@ namespace Param_ProjectName.Services
 {
     public class Localizer : ILocalizer
     {
-        private const string resourceName = "Param_ProjectName.Strings.AppResources";
-        static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(() => new ResourceManager(resourceName, IntrospectionExtensions.GetTypeInfo(typeof(Localizer)).Assembly));
+        private static string resourceName = GetResourceId();
+        private static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(() => new ResourceManager(resourceName, IntrospectionExtensions.GetTypeInfo(typeof(Localizer)).Assembly));
 
+        private static string GetResourceId()
+        {
+            string resourceName = null;
+            switch(Device.RuntimePlatform)
+            {
+                case Device.Android:
+                    resourceName = "Param_ProjectName.Android.Strings.AppResources";
+                    break;
+                case Device.iOS:
+                    resourceName = "Param_ProjectName.iOS.AppResources";
+                    break;
+                case Device.UWP:
+                    resourceName = "Param_ProjectName.UWP.Strings.AppResources";
+                    break;
+            }
 
+            return resourceName;
+        }
         public string GetStringForKey(string key)
         {
             string text = string.Empty;
